21e65a80a54108d9d155401a9ce1d0897bde296f
MadlenaSophie <madlenalindenthal@gmail.com>
2024-04-05 12:04:31 +0200

    enemies new

--- Box_Prototypy/Assets/Scripts/NewPlayerMovement.cs
+++ Box_Prototypy/Assets/Scripts/NewPlayerMovement.cs
@@ -2,7 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class PlayerMovement : MonoBehaviour
+public class NewPlayerMovement : MonoBehaviour
 {
     Rigidbody2D rb;
     float dirX, moveSpeed = 5f;
