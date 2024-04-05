a38d55a80c562e8b0d9889cc4b4d8a234c276d32
MadlenaSophie <madlenalindenthal@gmail.com>
2024-04-05 11:18:27 +0200

    jump through platforms

--- Box_Prototypy/Assets/Scripts/NewPlayerMovement.cs
+++ Box_Prototypy/Assets/Scripts/NewPlayerMovement.cs
@@ -6,36 +6,50 @@ public class NewPlayerMovement : MonoBehaviour
 {
     Rigidbody2D rb;
     float dirX, moveSpeed = 5f;
+    bool isGrounded; // Flag to track if the player is grounded
+    Transform platform; // Reference to the moving platform the player is on
+
     // Start is called before the first frame update
     void Start()
     {
-        rb = GetComponent<Rigidbody2D> ();   
+        rb = GetComponent<Rigidbody2D>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        dirX = Input.GetAxisRaw ("Horizontal") * moveSpeed;
-        if (Input.GetButtonDown("Jump") && rb.velocity.y == 0)
-            rb.AddForce (Vector2.up * 400f);  
+        dirX = Input.GetAxisRaw("Horizontal") * moveSpeed;
+
+        if (Input.GetButtonDown("Jump") && isGrounded)
+        {
+            rb.AddForce(Vector2.up * 400f);
+            isGrounded = false; // Player is no longer grounded after jumping
+        }
     }
 
-    void FixedUpdate ()
+    void FixedUpdate()
     {
-        rb.velocity = new Vector2 (dirX, rb.velocity.y);
+        rb.velocity = new Vector2(dirX, rb.velocity.y);
     }
 
     void OnCollisionEnter2D(Collision2D col)
     {
-        if(col.gameObject.name.Equals("MovingPlatform"))
-            this.transform.parent = col.transform;
+        if (col.gameObject.CompareTag("Ground") || col.gameObject.CompareTag("MovingPlatform"))
+        {
+            platform = col.transform;
+            transform.SetParent(platform); // Parent the player to the platform
+            isGrounded = true; // Player is grounded when colliding with the ground or platform
+        }
     }
 
     void OnCollisionExit2D(Collision2D col)
     {
-        if(col.gameObject.name.Equals("MovingPlatform"))
+        if (col.gameObject.CompareTag("Ground") || col.gameObject.CompareTag("MovingPlatform"))
         {
-            this.transform.parent = null;
+            transform.SetParent(null); // Unparent the player from the platform
+            platform = null;
+            isGrounded = false; // Player is no longer grounded when leaving the ground or platform
         }
     }
 }
+
