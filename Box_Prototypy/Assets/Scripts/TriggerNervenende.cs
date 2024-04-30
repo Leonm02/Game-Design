b110cb106db42f5a0313a7bb117d8da6b2815fa6
Leonm02 <75570949+Leonm02@users.noreply.github.com>
2024-04-30 10:40:04 +0200

    position popuptext

--- Box_Prototypy/Assets/Scripts/TriggerNervenende.cs
+++ Box_Prototypy/Assets/Scripts/TriggerNervenende.cs
@@ -1,15 +1,18 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class TriggerNervenende : MonoBehaviour
 {
     public GameObject popupPunishmentTimePrefab;
+    public Text timerText;
+    public Text textObject;
 
     // Start is called before the first frame update
     void Start()
     {
-        
+        textObject = GameObject.Find("Timer_Text").GetComponent<Text>();
     }
 
     // Update is called once per frame
@@ -19,14 +22,24 @@ public class TriggerNervenende : MonoBehaviour
     }
 
     private void OnCollisionEnter2D(Collision2D collision)
-    {
+    { 
+
+        // Beispiel: Position des Textobjekts abrufen und ausgeben
+        Vector3 textPosition = textObject.transform.position;
+        textPosition.x -= 0.8f;
+        textPosition.y -= 0.8f;
         Timer.currentTime -= 5f;
-        Instantiate(popupPunishmentTimePrefab, transform.position, Quaternion.identity);
+        Instantiate(popupPunishmentTimePrefab, textPosition, Quaternion.identity);
     }
 
     private void OnTriggerEnter2D(Collider2D collision)
     {
+
+        // Beispiel: Position des Textobjekts abrufen und ausgeben
+        Vector3 textPosition = textObject.transform.position;
+        textPosition.x -= 0.8f;
+        textPosition.y -= 0.8f;
         Timer.currentTime -= 5f;
-        Instantiate(popupPunishmentTimePrefab, transform.position, Quaternion.identity);
+        Instantiate(popupPunishmentTimePrefab, textPosition , Quaternion.identity);
     }
 }
