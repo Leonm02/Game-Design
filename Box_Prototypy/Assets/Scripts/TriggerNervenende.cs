93c0da4abf77ae5c5e72b8818184ab7e3c8452b7
Leonm02 <75570949+Leonm02@users.noreply.github.com>
2024-05-07 11:49:13 +0200

    Fixed punishment Time

--- Box_Prototypy/Assets/Scripts/TriggerNervenende.cs
+++ Box_Prototypy/Assets/Scripts/TriggerNervenende.cs
@@ -6,40 +6,40 @@ using UnityEngine.UI;
 public class TriggerNervenende : MonoBehaviour
 {
     public GameObject popupPunishmentTimePrefab;
-    public Text timerText;
     public Text textObject;
+    public Transform canvasTransform; 
 
     // Start is called before the first frame update
     void Start()
     {
         textObject = GameObject.Find("Timer_Text").GetComponent<Text>();
+        canvasTransform = GameObject.Find("In-Game UI").transform; 
     }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
 
     private void OnCollisionEnter2D(Collision2D collision)
-    { 
-
+    {
         // Beispiel: Position des Textobjekts abrufen und ausgeben
         Vector3 textPosition = textObject.transform.position;
         textPosition.x -= 0.8f;
         textPosition.y -= 0.8f;
         Timer.currentTime -= 5f;
-        Instantiate(popupPunishmentTimePrefab, textPosition, Quaternion.identity);
+
+        // Popup erstellen und dem Canvas als Kind hinzufügen
+        GameObject popup = Instantiate(popupPunishmentTimePrefab, textPosition, Quaternion.identity);
+        popup.transform.SetParent(canvasTransform, false);
     }
 
     private void OnTriggerEnter2D(Collider2D collision)
     {
-
         // Beispiel: Position des Textobjekts abrufen und ausgeben
         Vector3 textPosition = textObject.transform.position;
         textPosition.x -= 0.8f;
         textPosition.y -= 0.8f;
         Timer.currentTime -= 5f;
-        Instantiate(popupPunishmentTimePrefab, textPosition , Quaternion.identity);
+
+        // Popup erstellen und dem Canvas als Kind hinzufügen
+        GameObject popup = Instantiate(popupPunishmentTimePrefab, textPosition, Quaternion.identity);
+        popup.transform.SetParent(canvasTransform, false);
     }
 }
