affbd213a2608135e06e4694f65262363c69c717
MadlenaSophie <madlenalindenthal@gmail.com>
2024-05-16 21:50:09 +0200

    alte scripts gelöscht

--- Box_Prototypy/Assets/Scripts/NextLevelLoad.cs
+++ Box_Prototypy/Assets/Scripts/NextLevelLoad.cs
@@ -3,7 +3,6 @@ using UnityEngine.SceneManagement;
 
 public class NextLevelButton : MonoBehaviour
 {
-    public GameObject playerPrefab; // Drag and drop your player prefab here in the Unity Editor
     public string NameOfLevel;
     
     public void LoadNextLevel()
@@ -12,24 +11,4 @@ public class NextLevelButton : MonoBehaviour
         SceneManager.LoadScene(NameOfLevel);
     }
 
-    /*void Start()
-    {
-        SceneManager.sceneLoaded += OnSceneLoaded;
-    }
-
-    void OnSceneLoaded(Scene scene, LoadSceneMode mode)
-    {
-        if (scene.name == NameOfLevel)
-        {
-            Debug.Log("Loading scene: " + scene.name);
-
-            // Instantiate player prefab
-            GameObject player = Instantiate(playerPrefab, Vector3.zero, Quaternion.identity);
-            Debug.Log("Player instantiated at: " + player.transform.position);
-            // Optionally, you can set the player's position here
-            player.transform.position = new Vector3(-7, -3, 0);
-            
-
-        }
-    }*/
 }
